f94689110cafb83a73433be9c9111351db83a732
FighterSoul <hedi.abdessalem@gmail.com>
2023-12-21 02:16:25 +0100

    profile info done, still need to save profile pic and add feed and create post

--- socialapp/src/app/profile/profile/profile.component.ts
+++ socialapp/src/app/profile/profile/profile.component.ts
@@ -2,10 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { SharedService } from 'src/app/shared.service';
-import { HttpClient } from '@angular/common/http';
-
-
-
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-profile',
@@ -15,59 +12,81 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-
-  ngOnInit(): void {
-    // Initialization logic here
-  }
-
   username: string = '';
-  email: string = '';
+  email: string = '';  // Add this line
   major: string = '';
   quote: string = '';
-  courses: string[] = [];
-  interests: string[] = [];
+  courses: string = '';
+  interests: string = '';
   showForm: boolean = false;
   profilePic: File | null = null;
+  profilePicUrl: string = '../../assets/images/profilepic.png'; // Default image
 
   constructor(private sharedService: SharedService, private http: HttpClient) {
     this.username = this.sharedService.getSharedVariable();
-    console.log('Shared variable:', this.sharedService.getSharedVariable());
-
     this.email = this.sharedService.getSecondSharedVariable();
-    console.log('Second shared variable:', this.sharedService.getSecondSharedVariable());
-
-    console.log(this.username);
-    console.log(this.email);
   }
 
-  onFileSelected(event: Event) {
+  ngOnInit(): void {
+    this.http.get('http://localhost/freshstart/socialapp/src/app/profile/profile/profile.php', {
+      params: new HttpParams().set('Username', this.username)
+    }).subscribe((data: any) => {
+      this.username = data.Username;
+      this.email = data.Email;  // Update this line
+      this.major = data.major;
+      this.quote = data.quote;
+      this.courses = data.courses;
+      this.interests = data.interests;
+      this.profilePicUrl = data['Profile-Picture']; // replace with the actual property name for the profile picture
+    });
+  }
+
+  uploadProfilePic() {
+    const formData = new FormData();
+    if (this.profilePic) {
+      formData.append('profilePic', this.profilePic);
+      formData.append('username', this.username);
+  
+      this.http.post('http://localhost/freshstart/socialapp/src/app/profile/profile/profile.php', formData)
+        .subscribe({
+          next: (response: any) => {
+            console.log('Server response: ', response);
+            window.alert('Profile picture upload successful');
+          },
+          error: (error: any) => {
+            console.error('There was an error!', error);
+          }
+        });
+    }
+  }
+
+  onFileChange(event: Event) {
     const inputElement = event.target as HTMLInputElement;
     if (inputElement.files && inputElement.files.length > 0) {
-      this.profilePic = inputElement.files[0]; // Update profilePic with the selected file
-    } else {
-      this.profilePic = null; // If no file is selected, set profilePic to null
+      this.profilePic = inputElement.files[0];
+      this.profilePicUrl = URL.createObjectURL(inputElement.files[0]);
+      this.uploadProfilePic(); // upload the profile picture immediately after it's selected
     }
   }
 
   submitForm() {
     const formData = new FormData();
+    formData.append('username', this.username);
+    formData.append('email', this.email);  // Add this line
     formData.append('major', this.major);
     formData.append('quote', this.quote);
-    formData.append('courses', this.courses.join(', '));
-    formData.append('interests', this.interests.join(', '));
-    if (this.profilePic) {
-      formData.append('profilePic', this.profilePic);
-    }
-
-    this.http.post('http://localhost/profile.php', formData)
-      .subscribe(response => {
-        // handle successful response
-        console.log(response);
-      }, error => {
-        // handle error response
-        console.error(error);
-      });
-
-    this.showForm = false;
+    formData.append('courses', this.courses);
+    formData.append('interests', this.interests);
+    
+    this.http.post('http://localhost/freshstart/socialapp/src/app/profile/profile/profile.php', formData).subscribe({
+      next: response => {
+        console.log('Server response: ', response);
+        window.alert('Profile update successful');
+        location.reload(); // reload the page
+      },
+      error: error => {
+        console.error('There was an error!', error);
+      }
+    });
   }
 }
--- socialapp/src/app/profile/profile/profile.php
+++ socialapp/src/app/profile/profile/profile.php
@@ -1,38 +1,81 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
 
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    return 0;
+}
+
+$conn = new PDO("mysql:host=localhost;dbname=social-app", 'root', '');
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     try {
-        $conn = new PDO("mysql:host=localhost;dbname=social-app", 'root', '');
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        // Log the $_POST variable
-        error_log(print_r($_POST, true));
-
-        if(isset($_POST['username']) && isset($_POST['major']) && isset($_POST['quote']) && isset($_POST['courses']) && isset($_POST['interests'])){
+        if (isset($_POST['username']) && isset($_POST['major']) && isset($_POST['quote']) && isset($_POST['courses']) && isset($_POST['interests'])) {
             $username = $_POST['username'];
             $major = $_POST['major'];
             $quote = $_POST['quote'];
             $courses = $_POST['courses'];
             $interests = $_POST['interests'];
 
-            $sql = "UPDATE users SET major = :major, quote = :quote, courses = :courses, interests = :interests WHERE username = :username";
+            // Check if a file is uploaded
+            if (isset($_FILES['profilePic']) && $_FILES['profilePic']['error'] === UPLOAD_ERR_OK) {
+                // Save the uploaded file to a directory
+                $target_dir = "../../../uploads/";
+                $target_file = $target_dir . basename($_FILES["profilePic"]["name"]);
+                move_uploaded_file($_FILES["profilePic"]["tmp_name"], $target_file);
+
+                // Save the file path to the database
+                $sql = "UPDATE users SET major = :major, quote = :quote, courses = :courses, interests = :interests, Profile_Picture = :profilePic WHERE Username = :username";
+                $stmt = $conn->prepare($sql);
+
+                $stmt->bindParam(':username', $username);
+                $stmt->bindParam(':major', $major);
+                $stmt->bindParam(':quote', $quote);
+                $stmt->bindParam(':courses', $courses);
+                $stmt->bindParam(':interests', $interests);
+                $stmt->bindParam(':profilePic', $target_file);
+
+                $stmt->execute();
+
+                echo json_encode(['message' => 'Profile update successful']);
+            } else {
+                // No file uploaded, update profile information without changing the Profile_Picture column
+                $sql = "UPDATE users SET major = :major, quote = :quote, courses = :courses, interests = :interests WHERE Username = :username";
+                $stmt = $conn->prepare($sql);
+
+                $stmt->bindParam(':username', $username);
+                $stmt->bindParam(':major', $major);
+                $stmt->bindParam(':quote', $quote);
+                $stmt->bindParam(':courses', $courses);
+                $stmt->bindParam(':interests', $interests);
+
+                $stmt->execute();
+
+                echo json_encode(['message' => 'Profile update successful']);
+            }
+        }
+    } catch (PDOException $e) {
+        echo json_encode(['message' => 'Error: ' . $e->getMessage()]);
+    }
+} elseif ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    try {
+        if (isset($_GET['Username'])) {
+            $username = $_GET['Username'];
+
+            $sql = "SELECT * FROM users WHERE Username = :username";
             $stmt = $conn->prepare($sql);
 
-            $stmt->bindValue(':username', $username);
-            $stmt->bindValue(':major', $major);
-            $stmt->bindValue(':quote', $quote);
-            $stmt->bindValue(':courses', $courses);
-            $stmt->bindValue(':interests', $interests);
+            $stmt->bindParam(':username', $username);
 
             $stmt->execute();
 
-            echo json_encode(['message' => 'Profile updated successfully']);
-        } else {
-            echo json_encode(['message' => 'Please provide all profile details']);
+            $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            echo json_encode($user);
         }
-    } catch(PDOException $e) {
+    } catch (PDOException $e) {
         echo json_encode(['message' => 'Error: ' . $e->getMessage()]);
     }
 }
